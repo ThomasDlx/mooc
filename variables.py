673930e6e3ac7faf9c6b1d8cc6fc1019fb1aaa43
ThomasDlx <thomas.delcroix1303@outlook.fr>
2024-01-30 09:30:21 +0100

    Add exercice variable

---
+++ variables.py
@@ -0,0 +1,34 @@
+a = 12
+b = "text"
+c = 1.2
+d = True
+e = None
+l = [1,2,3,4,5]
+t = (1,2,3,4,5)
+
+print("Type de a :", type(a))
+print("Type de b :", type(b))
+print("Type de c :", type(c))
+print("Type de d :", type(d))
+print("Type de e :", type(e))
+print("Type de l :", type(l))
+print("Type de t :", type(t))
+
+# Python know type of variable, useless to declare it but it's more readable if you do it
+# You can change type of variable
+# You can display variable with print function
+
+print("-------------")
+print("| Exercices |")
+print("-------------")
+# Exercice
+# Create a variable with your name and display it
+
+
+# Write a variable with you age and display it
+
+
+# Write a variable with your size and display it (float only)
+
+# Write a liste with your name, age and size and display it
+# If you want to save a variable of many person for example, you can use list
